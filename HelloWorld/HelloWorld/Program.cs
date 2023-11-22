9acbf62a4e0f9f6ef3bb411130abab37600e8d80
Eatler <chd@mail.ru>
2023-11-22 15:47:32 +0700

    Test commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        string c = Console.ReadLine();
+
+        if (c == "+")
+        {
+            Console.WriteLine(a + b); return;
+        }else if (c == "-")
+        {
+            Console.WriteLine(a - b); return;
+        }else if (c == "*")
+        {
+            Console.WriteLine(a * b); return;
+        }else if (c == "/")
+        {
+            Console.WriteLine(a / b); return;
+        }
+        else
+        {
+            Console.WriteLine("Ничего не понял");
+        }
+    }
+}
